20775c59bb296f114f8431d709af529ce40be2bb
mercutio22 <mnemonico@posthocergopropterhoc.net>
2013-05-22 07:00:07 -0300

    new function to deal with the repetition in the code generating the column-side colors

--- code.analysis.Hugo.April2013.R
+++ code.analysis.Hugo.April2013.R
@@ -91,6 +91,7 @@ colnames(Src.signature) = c('ProbeID', 'GeneSymbol', 'Description', 'LocusLink',
 Src.sig$direction <- NA
 Src.signature$direction[Src.signature$FoldChange<=1] <- c("DN.BILD") # just to denote this info is not from our analysis (BILD) 
 Src.signature$direction[Src.signature$FoldChange>1] <- c("UP.BILD")
+
 ##src signature #Bild et al. 2006
 src <- read.delim(file="bild.src.txt") #where did this file come from? Maybe GSE3151
 length(intersect(src[,1], dimnames(tothill.d)[[1]]))
@@ -104,7 +105,7 @@ tothill.src.probe <- intersect(src[,1], dimnames(tothill.d)[[1]])
 gse7305.src.probe <- intersect(src[,1], dimnames(gse7305.d)[[1]])
 gse7307.src.probe <- intersect(src[,1], dimnames(gse7307.d)[[1]])
 gse6364.src.probe <- intersect(src[,1], dimnames(gse6364.d)[[1]])
-#subset data to only SRC specific and create dataframe.
+#subset data to only SRC specific and create dataframe. 
 tothill.src <- tothill.d[tothill.src.probe,]; tothill.src <- as.data.frame(tothill.src)
 gse6008.src <- gse6008.d[gse6008.src.probe,]; gse6008.src <- as.data.frame(gse6008.src)
 gse6364.src <- gse6364.d[gse6364.src.probe,]; gse6364.src <- as.data.frame(gse6364.src)
@@ -142,6 +143,7 @@ gse7305.nnn <- as.data.frame(as.character(unlist(lapply((gse7305.nn), "[", 1) ))
 gse7305.s$characteristics_ch1a <- gse7305.nnn[,1]
 
 
+#This repetitive block I will refactor into a function.
 tothill.s.sort <- sort.data.frame(tothill.s[,c("geo_accession","characteristics_ch1a")], 
 	key = "characteristics_ch1a")
 gse6008.s.sort <- sort.data.frame(gse6008.s[,c("geo_accession","characteristics_ch1a")], 
@@ -196,22 +198,63 @@ color.map.gse6364.type <- function(mol.biol) {
 #rowsidecolors 
 bildColors.colorize <- function(abbrv){ 
 	if (abbrv == 'UP.BILD') 'red'
-	else if (abbrv == 'DN.BILD') 'blue'
+	else if (abbrv == 'DN.BILD') 'green'
 	else '#FFFFFF' #insignificant cases 
 } 
 
 klColors.colorize = function(abbrv){
-	if (abbrv == 'UP.KL') 'red'
-	else if (abbrv == 'DN.KL') 'blue'
-	else '#FFFFFF' #insignificant cases
+	if (abbrv == 'UP.KL') {return('red')}
+	else if (abbrv == 'DN.KL') {return('green')}
+	else return('#FFFFFF') #insignificant cases
 }
 
+build.side.map = function(df){
+	### takes dataframe, merges with the Src signature and creates RowSideColors for our plots
+	## df is the full dataframe, with FC and P values
+	df$direction = NA
+	df$direction[df$FC<0]  <- c("DN.KL") 
+	df$direction[df$FC>=0] <- c("UP.KL")
+	df$id = dimnames(df)[[1]]
+	df$significance[df$p.value<=0.05] <- c("Significant")
+	src.sig <- merge(df, Src.signature, by.x = 'id', by.y = 'ProbeID', all.x = T) 
+	#I want the white color to mark the statistically insignificant cases, so:
+	src.sig$direction.x[which(is.na(src.sig$significance))] <- 'insignificant'
+	bildColors = unlist(lapply(src.sig$direction.y, bildColors.colorize))
+	klColors = unlist(lapply(src.sig$direction.x, klColors.colorize))
+	colors = cbind(bildColors,klColors)
+	colnames(colors) = c('Bild et al.', 'Lawrenson et al.')
+	return(colors)
+}
+
+## WORKING HERE ##TODO: This sux. Should need fewer parameters
+build.top.map = function(df, df.s, df.colormap.function){
+	#builds the heatmap top rows for helping group visualization
+	## df is the full dataframe, with FC and P values
+	## df.colormap.function maps metadata with colors
+	cols = as.matrix(colnames(df[1:(ncol(df)-4)]))
+	df.src.type = apply(cols,1,func.list$vlookup,df.s[,c("Sample","characteristics_ch1a")],"characteristics_ch1a")
+	df.src.type = as.character(df.src.type)
+	df.src.type[is.na(df.src.type)] = c('0')
+	cc.df.src.type = unlist(lapply(df.src.type, df.colormap.function))
+	ColSideColors = matrix(as.character(c(cc.df.src.type)), 
+			nrow = length(cc.df.src.type),
+			ncol = 1)
+	ColSideColors = cbind(ColSideColors, ColSideColors) #a trick to make the top plot part thicker 
+	return(ColSideColors)
+}
+
+
+####################################################### THIS BLOCK WILL BE GONE #################
+#################################################################################################
 #tothill.type
-tothill.src.type<-apply(as.matrix(names(tothill.src[,tothill.s.sort[,1]])),1,func.list$vlookup,tothill.s[,c("geo_accession","characteristics_ch1a")],"characteristics_ch1a")
+tothill.src.type<-apply(as.matrix(names(tothill.src[,tothill.s.sort[,1]])),
+	1,
+	func.list$vlookup,
+	tothill.s[,c("geo_accession","characteristics_ch1a")],
+	"characteristics_ch1a")
 tothill.src.type<-as.character(tothill.src.type)
 tothill.src.type[is.na(tothill.src.type)]<-c("0")
 cc.tothill.src.type<-unlist(lapply(tothill.src.type, color.map.tothill.type))
-
 cc.col.t<-matrix(as.character(c(cc.tothill.src.type)), nrow=285,ncol=1)
 colnames(cc.col.t) = c("Tothill")
 cc.col.t <- cbind(cc.col.t, cc.col.t)
@@ -225,7 +268,6 @@ cc.gse6008.src.type<-unlist(lapply(gse6008.src.type, color.map.gse6008.type))
 cc.col.gse6008<-matrix(as.character(c(cc.gse6008.src.type)), nrow=103,ncol=1)
 colnames(cc.col.gse6008) = c("GSE6008")
 cc.col.gse6008 <- cbind(cc.col.gse6008, cc.col.gse6008)
-
 
 #gse6364.type ##PROBLEM: the resulting dataframe is 37 by 2 - heatmap expects 41 by 2
 gse6364.src.type<-apply(as.matrix(names(gse6364.src[,gse6364.s.sort[,1]])),1,func.list$vlookup,gse6364.s[,c("geo_accession","characteristics_ch1a")],"characteristics_ch1a")
@@ -256,7 +298,6 @@ cc.col.gse7305<-matrix(as.character(c(cc.gse7305.src.type)), nrow=20,ncol=1)
 colnames(cc.col.gse7305) = c("GSE7305")
 cc.col.gse7305 <- cbind(cc.col.gse7305, cc.col.gse7305)
 
-
 #gse7307.type
 gse7307.src.type<-apply(
 	as.matrix(names(gse7307.src[,as.character(gse7307.s.sort[,1])])),
@@ -272,7 +313,8 @@ cc.gse7307.src.type<-unlist(lapply(gse7307.src.type, color.map.gse7307.type))
 cc.col.gse7307<-matrix(as.character(c(cc.gse7307.src.type)), nrow=134,ncol=1)
 colnames(cc.col.gse7307) = c("GSE7307")
 cc.col.gse7307 <- cbind(cc.col.gse7307, cc.col.gse7307)
-
+#######################################################################################
+################################GONE###################################################
 
 
 #cc.col.merged <- rbind(cc.col.t, cc.col.gse6008)
@@ -300,7 +342,12 @@ tothill.hv<-heatmap.plus(
 		cexCol=0.6,
 		keysize=1,
 		dendrogram=c("none"),
-		main = paste("Tothill; SRC only (73 probes) ",dim(tothill.src)[1],"Probes; ",dim(tothill.src)[2],"Samples"),
+		main = paste("Tothill; SRC only (73 probes) ",
+			dim(tothill.src)[1],
+			"Probes; ",
+			dim(tothill.src)[2],
+			"Samples"
+			),
 		#labCol=NA
 		labRow=NA
 )
@@ -326,12 +373,15 @@ gse6008.hv<-heatmap.plus(
 		cexCol=0.6,
 		keysize=1,
 		dendrogram=c("none"),
-		main = paste("gse6008; SRC only (73 probes) ",dim(gse6008.src)[1],"Probes; ",dim(gse6008.src)[2],"Samples"),
+		main = paste("gse6008; SRC only (73 probes) ",
+			dim(gse6008.src)[1],"Probes; ",
+			dim(gse6008.src)[2],
+			"Samples"
+			),
 		#labCol=NA
 		labRow=NA
-)
+		)
 dev.off()
-
 
 ## This block is new ####
 tttt <- as.matrix(gse7305.src[,as.character(gse7305.s.sort[,1])])
@@ -345,21 +395,6 @@ tttt$p.value <- as.numeric(pv)
 tttt$FC <- tttt$mean.T - tttt$mean.N
 func.list$volcano(tttt, fold.change.col = "FC", pv.col = "p.value", title.plot= "Volcano Plot, Endometrium vs Normal (GSE7305)", cut.line = -log10(0.05), foldcut = 0.5) 
 
-build.side.columns = function(df){
-	###This function takes our dataframe, merges with the Src signature and creates RowSideColors for our plots
-	df$direction = NA
-	df$direction[df$FC<0]  <- c("DN.KL") 
-	df$direction[df$FC>=0] <- c("UP.KL")
-	df$id = dimnames(df)[[1]]
-	df$significance[df$p.value<=0.05] <- c("Significant")
-	src.sig <- merge(df, Src.signature, by.x = 'id', by.y = 'ProbeID', all.x = T) 
-	#I want the white color to mark the statistically insignificant cases, so:
-	src.sig$direction.x[which(is.na(src.sig$significance))] <- 'insignificant'
-	bildColors = unlist(lapply(src.sig$direction.y, bildColors.colorize))
-	klColors = unlist(lapply(src.sig$direction.x, klColors.colorize))
-	colors = cbind(bildColors,klColors)
-	return(colors)
-}
 #this table compares our analysis with Bilds's
 tcomparison <- table(subset(src.sig, label == "Significant")$direction.y, subset(src.sig, label == "Significant")$direction.x)
 
@@ -373,8 +408,14 @@ gse7305.hv<-heatmap.plus(
 		na.rm=TRUE,
 		scale="none",
 		#RowSideColor=probe.cc,
-		ColSideColors=cc.col.gse7305,
-		RowSideColors=build.side.columns(tttt), # ours, and Bild et al.'s most differentially expressed genes
+		#ColSideColors=cc.col.gse7305,
+		ColSideColors=build.top.map(gse7305.src,
+			gse7305.s,
+			gse7305.s.sort, 
+			label=GSE7305,
+			color.map.gse7305.type
+			),
+		RowSideColors=build.side.map(tttt), # most differentially expressed genes
 		col=jet.colors(75),
 		#key=FALSE,
 		symkey=FALSE,
@@ -386,7 +427,12 @@ gse7305.hv<-heatmap.plus(
 		cexCol=0.6,
 		keysize=1,
 		dendrogram=c("none"),
-		main = paste("gse7305; SRC only (73 probes) ",dim(gse7305.src)[1],"Probes; ",dim(gse7305.src)[2],"Samples"),
+		main = paste("gse7305; SRC only (73 probes) ",
+			dim(gse7305.src)[1],
+			"Probes; ",
+			dim(gse7305.src)[2],
+			"Samples"
+			),
 		#labCol=NA
 		labRow=NA
 )
@@ -395,43 +441,47 @@ png(filename = "gse7307.src.png", bg="white", res=300, width=3000, height=3000)
 tt <- as.matrix(gse7307.src[,as.character(gse7307.s.sort[,1])])
 tt <- tt[-4,] # removed "1556499_s_at"
 tt <- as.data.frame(tt);
-tt <- log2(tt);
 gse7307.norm <- as.character(subset(gse7307.s.sort, characteristics_ch1a=="endometrium")$Sample)
 gse7307.tum <- as.character(subset(gse7307.s.sort, characteristics_ch1a=="endometrium/ovary")$Sample)
-tt.n <- apply(tt[,gse7307.norm], 1, mean, na.rm=T); tt$mean.N <- tt.n
-tt.t <- apply(tt[,gse7307.tum], 1, mean, na.rm=T); tt$mean.T <- tt.t
+tt = cbind(tt[,gse7307.tum], tt[,gse7307.norm]) ##This might mess up the probe sorting
+tt <- log2(tt);
 pv.tt <- apply(tt, 1, func.list$studentT, s1=c(gse7307.norm),s2=c(gse7307.tum))
 tt$p.value <- as.numeric(pv.tt)
+tt.n <- apply(tt[,gse7307.norm], 1, mean, na.rm=T); tt$mean.N <- tt.n
+tt.t <- apply(tt[,gse7307.tum], 1, mean, na.rm=T); tt$mean.T <- tt.t
 tt$FC <- tt$mean.T - tt$mean.N
-func.list$volcano(tt, fold.change.col = "FC", pv.col = "p.value", title.plot= "Volcano Plot, Endometrium/Ovary vs Normal (GSE7307)", cut.line = -log10(0.05), foldcut = 0.5) # Fails silently??
-
+func.list$volcano(tt, fold.change.col = "FC", pv.col = "p.value", title.plot= "Volcano Plot, Endometrium/Ovary vs Normal (GSE7307)", cut.line = -log10(0.05), foldcut = 0.5)
 gse7307.hv<-heatmap.plus(
-		#as.matrix(temp[hv1[[1]],(as.character(cl.sort[,1]))]),
-		#as.matrix(temp[,(as.character(cl.sort[,1]))]),
-		#as.matrix(gse7307.src[,as.character(gse7307.s.sort[,1])]), 
-		as.matrix(tt[,1:(ncol(tt)-4)]),
-		#temp.cc,
-		na.rm=TRUE,
-		scale="none",
-		#RowSideColor=probe.cc,
-		ColSideColors=cc.col.gse7307,
-		col=jet.colors(75),
-		#key=FALSE,
-		symkey=FALSE,
-		density.info="none",
-		trace="none",
-		Rowv=TRUE,
-		Colv=NA,
-		cexRow=1,
-		cexCol=0.6,
-		keysize=1,
-		dendrogram=c("none"),
-		main = paste("gse7307; SRC only (73 probes) ",dim(gse7307.src)[1],"Probes; ",dim(gse7307.src)[2],"Samples"),
-		#labCol=NA
-		labRow=NA
+	#as.matrix(temp[hv1[[1]],(as.character(cl.sort[,1]))]),
+	#as.matrix(temp[,(as.character(cl.sort[,1]))]),
+	#as.matrix(gse7307.src[,as.character(gse7307.s.sort[,1])]), 
+	as.matrix(tt[,1:(ncol(tt)-4)]),
+	#temp.cc,
+	na.rm=TRUE,
+	scale="none",
+	#RowSideColor=probe.cc,
+	ColSideColors=build.top.map(tt, gse7307.s, color.map.gse7307.type),
+	RowSideColors=build.side.map(tt),
+	col=jet.colors(75),
+	#key=FALSE,
+	symkey=FALSE,
+	density.info="none",
+	trace="none",
+	Rowv=TRUE,
+	Colv=NA,
+	cexRow=1,
+	cexCol=0.6,
+	keysize=1,
+	dendrogram=c("none"),
+	main = paste("gse7307; SRC only (73 probes) ",
+		dim(tt[,1:(ncol(tt)-4)])[1],
+		"Probes; ",
+		 dim(tt[,1:(ncol(tt)-4)])[2],
+		"Samples"),
+	#labCol=NA
+	labRow=NA
 )
 dev.off()
-
 
 png(filename = "gse6364.src.png", bg="white", res=300, width=3000, height=3000)
 ttt <- as.matrix(gse6364.src[,as.character(gse6364.s.sort[,1])])
@@ -445,7 +495,6 @@ ttt <- as.data.frame(ttt);
 #ttt$p.value <- as.numeric(pv.ttt)
 #ttt$FC <- ttt$mean.T - ttt$mean.N
 #func.list$volcano(ttt, fold.change.col = "FC", pv.col = "p.value", title.plot= "Volcano Plot, Endometrium/Ovary vs Normal (GSE6364)", cut.line = -log10(0.05), foldcut = 0.5)
-### PROBLEM 'ColSideColors' dim()[2] must be of length ncol(x)
 gse6364.hv<-heatmap.plus(
 		#as.matrix(temp[hv1[[1]],(as.character(cl.sort[,1]))]),
 		#as.matrix(temp[,(as.character(cl.sort[,1]))]),
